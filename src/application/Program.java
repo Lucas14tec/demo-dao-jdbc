f1451f4caad77dc15291f5a9b1855cfe207792c7
Lucas14tec <lucas.tecdantas@gmail.com>
2022-11-04 10:58:06 -0300

    DaoFactory e SellerDaoJDBC criadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import model.entities.Department;
 import model.entities.Seller;
+import model_dao.DaoFactory;
+import model_dao.SellerDao;
 
 import java.util.Date;
 
@@ -9,6 +11,8 @@ public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1, "Books");
         Seller seller = new Seller(1 , "Leandro", "EMAIL@PÃO", new Date(), 220.50, obj  );
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
